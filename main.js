462b4531ae0fb459171a7ef2f8cc1dd9d2ca8c67
BraxtonKrusemark <krusemarkbraxton@gmail.com>
2025-07-14 17:27:55 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// Grab elements
+
+//Nav styles on scroll
+window.addEventListener("scroll", () => {
+    const header = document.getElementById("header");
+    if (window.scrollY > 50) {
+        header.classList.add("shrink");
+    } else {
+        header.classList.remove("shrink");
+    }
+});
+// Open menu & search pop-up
+
+// Open/Close search form popup
+
+// -- Close the search form popup on ESC keypress
+
+// Switch theme/add to local storage
+
+// Swiper
